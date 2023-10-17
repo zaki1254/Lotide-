75f1cd216c7a64c9bff1976420275d8eb601406c
zaki1254 <zakessa10@gmail.com>
2023-10-17 09:29:16 -0400

    Return First Element of Array

---
+++ head.js
@@ -0,0 +1,13 @@
+const assertEqual = function (actual, expected){}
+
+ const head = function(arr) {
+  return arr[0]
+
+}
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
